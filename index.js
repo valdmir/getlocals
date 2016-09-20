f247f3d403eb774545f038c4f1236c30accd3a97
freddywijaya <vald.karate@gmail.com>
2016-09-20 22:26:52 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
