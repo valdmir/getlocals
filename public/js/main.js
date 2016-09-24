f47abcfe53290c0e65a635f8f00fcadc2c2a601e
freddywijaya <vald.karate@gmail.com>
2016-09-24 18:33:24 +0700

    change code

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
-  $('.carousel').carousel({
-		interval: 3000
-	});
+  $('.slick').slick();
+  $(window).scroll(function () {
+    console.log($(document).scrollTop() );
+
+    if($(document).scrollTop() > 10 &&$(document).scrollTop() <30 )
+    {
+
+        $("#logoblue").css({opacity:1});
+        $("#logowhite").css({opacity:0});
+
+        $("#header_nav p").css({color:"black"});
+      $("#header_background").animate({opacity:1},{
+      queue: false,
+      duration: 100
+    });
+
+    }
+    else if($(document).scrollTop() < 10 )
+    {
+      $("#logoblue").css({opacity:0});
+      $("#logowhite").css({opacity:1});
+      $("#header_nav p").css({color:"white"});
+      $("#header_background").animate({opacity:0},{
+      queue: false,
+      duration: 100
+    });
+
+    }
+    else if($(document).scrollTop()>30 &&$(document).scrollTop()<50){
+      $("#header_nav").animate({height:"100px"},{
+        queue: false,
+        duration: 300
+      });
+      $(".navbar-custom").animate({marginTop:"20px"},{
+        queue: false,
+        duration: 300
+      });
+    }
+    else if($(document).scrollTop()>50 ){
+      $("#header_nav").animate({height:"60px"},{
+        queue: false,
+        duration: 300
+      });
+      $(".navbar-custom").animate({marginTop:"0px"},{
+        queue: false,
+        duration: 300
+      });
+    }
+});
 });
